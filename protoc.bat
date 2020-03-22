659963694ebbeac1f967a955fed754f079871678
GrizzlyEntertainment <47308540+GrizzlyEntertainment@users.noreply.github.com>
2020-03-22 18:30:45 -0500

    Seperated Protocol Buffers to be in surrounding servers.

--- protoc.bat
+++ protoc.bat
@@ -4,6 +4,3 @@ start protoc -I="src/main/protobuf/" --java_out="src/main/java/" "src/main/proto
 start protoc -I="src/main/protobuf/" --java_out="src/main/java/" "src/main/protobuf/"proxy.proto
 start protoc -I="src/main/protobuf/" --java_out="src/main/java/" "src/main/protobuf/"sharding.proto
 start protoc -I="src/main/protobuf/" --java_out="src/main/java/" "src/main/protobuf/"shared.proto
-start protoc -I="src/main/protobuf/" --java_out="src/main/java/" "src/main/protobuf/"realm.proto
-start protoc -I="src/main/protobuf/" --java_out="src/main/java/" "src/main/protobuf/"world.proto
-start protoc -I="src/main/protobuf/" --java_out="src/main/java/" "src/main/protobuf/"channel.proto
